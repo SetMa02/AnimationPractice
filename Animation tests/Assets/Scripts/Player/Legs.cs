56214c7dbec617b626399c310c2fab8be7f74c6b
Максим Майнагашев <maxuser99@mail.ru>
2021-07-12 14:57:13 +0700

    Legs bone

---
+++ Animation tests/Assets/Scripts/Player/Legs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Legs : MonoBehaviour
+{
+    [SerializeField] private Transform _hip;
+    [SerializeField] private Transform _foot;
+    [SerializeField] private ContactFilter2D _filter;
+
+    private RaycastHit2D[] _hits = new RaycastHit2D[12];
+
+    private void LateUpdate()
+    {
+        var hitsCount = Physics2D.Raycast(_hip.position, Vector3.down, _filter, _hits);
+        if(hitsCount > 0)
+        {
+            var first = _hits[0];
+
+            _foot.position = first.point;
+            _foot.rotation = Quaternion.FromToRotation(Vector3.up, first.normal);
+        }
+    }
+
+}
